82d8c29614810fa3c822dfd15b405e3d5c7c3363
dvir zadok <dvir0934@gmail.com>
2026-02-16 14:48:30 +0200

    אני באמצע בניית הקלאסים

---
+++ app/src/main/java/com/example/myfinalproject/Trainee.java
@@ -0,0 +1,16 @@
+package com.example.myfinalproject;
+
+public class Trainee {
+
+    //מזהה ייחודי
+    long id;
+    String name;
+    int age;
+    double weight;
+    double highet;
+    double monthlyCost;      // כמה משלם כל חודש
+    double remainingDebt;    // כמה נשאר לשלם
+    int dayOfPayment;
+    double[] weightYearTracking; //בכל סוף שבוע הוא מעדכן את המשקל הנתון האחרון
+
+}
